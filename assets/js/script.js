9602843ececbaf7b0fe29f1bc8ab28567bd4eba0
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-12-04 21:20:57 -0500

    change click to onClick & add Input value to list logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 // check off todo by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 // click on X to delete list item
-$("span").click(function (e) {
+$("ul").on("click", "span", function (e) {
   $(this).parent().fadeOut(500, function (){ 
     $(this).remove();
-   });
+  });
   e.stopPropagation();
+});
+
+// create new todo
+$("input[type='text']").keypress(function(e) { 
+  if(e.which === 13){
+    // take the value from input
+    var todoText = $(this).val();
+    // clearout form input
+    $(this).val("");
+    // create new list item with value
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
