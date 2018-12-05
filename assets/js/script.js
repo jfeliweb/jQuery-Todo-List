146d8c7aea01bd8a12aeb53e1a48c59efa17e863
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-12-04 21:02:35 -0500

    add deleted list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ $("li").click(function () {
   $(this).toggleClass("completed");
 });
 
+// click on X to delete list item
+$("span").click(function (e) {
+  $(this).parent().fadeOut(500, function (){ 
+    $(this).remove();
+   });
+  e.stopPropagation();
+});
